fc1ebf39b6b208632a53b5f17a2acface1b7f1dd
dnhosemacs-svg <dnhosemacs@gmail.com>
2026-05-22 08:15:10 +0200

    filtos en store

--- src/stores/ui-store.ts
+++ src/stores/ui-store.ts
@@ -1,10 +1,22 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
+import type { ProductListQuery } from "@/lib/validators/product";
+
+const defaultProductFilters: ProductListQuery = {
+  search: undefined,
+  categoryId: undefined,
+  sortBy: "name",
+  sortOrder: "asc",
+};
 
 type UiState = {
   sidebarOpen: boolean;
   setSidebarOpen: (open: boolean) => void;
   toggleSidebar: () => void;
+
+  productFilters: ProductListQuery;
+  setProductFilters: (patch: Partial<ProductListQuery>) => void;
+  resetProductFilters: () => void;
 };
 
 export const useUiStore = create<UiState>()(
@@ -14,10 +26,17 @@ export const useUiStore = create<UiState>()(
       setSidebarOpen: (sidebarOpen) => set({ sidebarOpen }),
       toggleSidebar: () =>
         set((state) => ({ sidebarOpen: !state.sidebarOpen })),
+
+      productFilters: defaultProductFilters,
+      setProductFilters: (patch) =>
+        set((state) => ({
+          productFilters: { ...state.productFilters, ...patch },
+        })),
+      resetProductFilters: () => set({ productFilters: defaultProductFilters }),
     }),
     {
       name: "carpinteria-ui",
       partialize: (state) => ({ sidebarOpen: state.sidebarOpen }),
-    }
-  )
+    },
+  ),
 );
