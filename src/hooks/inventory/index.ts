7f239938a8a0e9200c6b9aadd019f7daf01794b3
dnhosemacs-svg <dnhosemacs@gmail.com>
2026-05-22 08:24:47 +0200

    exports de hooks de inventario

---
+++ src/hooks/inventory/index.ts
@@ -0,0 +1,13 @@
+export { useProductsQuery } from "./use-products-query";
+export { useCategoriesQuery } from "./use-categories-query";
+export {
+  useCreateProductMutation,
+  useUpdateProductMutation,
+  useDeleteProductMutation,
+} from "./use-product-mutations";
+export {
+  useCreateCategoryMutation,
+  useUpdateCategoryMutation,
+  useDeleteCategoryMutation,
+} from "./use-category-mutations";
+export { useUpdateStockMutation } from "./use-update-stock-mutation";
